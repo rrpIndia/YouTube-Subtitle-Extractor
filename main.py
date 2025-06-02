ff45cb73162c5283657aec19e7640eab62fb4360
Sartaj <ansarisartaj660@gmail.com>
2025-06-02 22:48:56 +0530

    added split_story function to split subtitles without cutting in middle

--- main.py
+++ main.py
@@ -101,6 +101,30 @@ def get_subtitle(vid_id) -> str:
         print("Error:", e)
 
 
+def split_story(story, max_words=46):
+    words = story.split()
+    parts = []
+    current_part = []
+    current_word_count = 0
+
+    for word in words:
+        if current_word_count + len(current_part) + 1 <= max_words:  # +1 for the space
+            current_part.append(word)
+            current_word_count += 1
+        else:
+            parts.append(" ".join(current_part))
+            current_part = [word]
+            current_word_count = 1
+    if current_part:
+        parts.append(" ".join(current_part))
+    return parts
+
+story_text = '''YFree download at http://rahulmehta'''
+story_parts = split_story(story_text)
+
+for part in story_parts:
+    print(part)
+
 
 # for a single session
 
